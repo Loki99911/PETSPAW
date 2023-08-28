7ae04ca42434c214c681e215f3752c4a1aef4f0f
Loki99911 <nikita_kresik@i.ua>
2023-08-28 09:24:35 +0300

    Update ModalUploadBtn.tsx

--- src/app/components/ModalUploadBtn/ModalUploadBtn.tsx
+++ src/app/components/ModalUploadBtn/ModalUploadBtn.tsx
@@ -1,18 +1,5 @@
-// "use client";
-// import { LinkComp } from "../LinksComp/LinkComp";
-// import "./ModalUploadBtn.scss";
-// export const ModalUploadBtn = ({ closeMenu }) => {
-//   return (
-//     <div className="modal__overlay">
-//       <span onClick={closeMenu} className="close__btn">
-//         <svg width="25" height="25" className="close__btn--icon">
-//           <use href="/symbol-defs.svg#icon-close-20"></use>
-//         </svg>
-//       </span>
-//       <LinkComp />
-//     </div>
-//   );
-// };
+"use client";
+
 import { useRef } from "react";
 import ModalUpload from "../ModalUpload/ModalUpload";
 import "./ModalUploadBtn.scss";
@@ -31,6 +18,7 @@ export default function ModalUploadBtn() {
         className="upload"
         onClick={onClickUpload}
         type="button"
+        // eslint-disable-next-line
         className="upload__btn"
       >
         <svg width="20" height="18" className="upload__btn--icon">
