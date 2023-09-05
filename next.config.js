3b336ff8241ef880bdfa459eac6e528eafbe3af5
Loki99911 <nikita_kresik@i.ua>
2023-09-05 17:41:46 +0300

    logo

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn2.thecatapi.com",
-        port: "",
-        pathname: "/images/**",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "cdn2.thecatapi.com",
+  //       port: "",
+  //       pathname: "/images/**",
+  //       u,
+  //     },
+  //   ],
+  // },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
